e090c0f6d05bde2f0eda5f3d93cfff951bd7d3cc
IronjuanU911 <jjarevalog@ufpso.edu.co>
2024-09-20 16:36:22 -0500

    AgeValidation.java

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,25 @@
-public class AgeValidation {
+public class Store {
     public static void main(String[] args) {
-        int age = 18;
+
+        int [][] productos = new int[2][3];
+
         
-        if (age >= 18) {
-            System.out.println("Access granted");
-        }
         
-        if (age < 18) {
-            System.out.println("Access denied");
+        int p1 = 15;
+        int p2 = 10;
+        int p3 = 5;
+
+        int total1 = p1 * 2;
+        int total2 = p2 * 3;
+        int total3 = p3 * 4;
+
+        int totalSales = total1 + total2 + total3;
+
+        if (totalSales > 50) {
+            System.out.println("Good sales performance");
+        } else {
+            System.out.println("Low sales performance");
         }
+
     }
 }
