4f04f5943a1ed86dce5682df77e79556b4056b4a
IronjuanU911 <jjarevalog@ufpso.edu.co>
2024-09-20 16:37:03 -0500

    AgeValidation.java

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,25 +1,18 @@
-public class Store {
+import java.util.Scanner; //Importamos la libreria scanner
+
+public class AgeValidation {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Creamos el objeto llamado sc, el cual utilizaremos para escanear la edad.
 
-        int [][] productos = new int[2][3];
+        final int limit_age = 18; //Establecemos la constante del limite de edad
 
+        System.out.println("Inserte la edad del usuario"); //Le pedimos al usuario la edad a evaluar
+        int age = sc.nextInt();
         
-        
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
-
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
-
-        int totalSales = total1 + total2 + total3;
-
-        if (totalSales > 50) {
-            System.out.println("Good sales performance");
-        } else {
-            System.out.println("Low sales performance");
+        if (age >= 18) { //Definimos si el usuario es mayor de edad o no, para luego definir si accedera al sistema o no
+            System.out.println("Access granted");
+        }else{
+            System.out.println("Access denied");
         }
-
     }
 }
